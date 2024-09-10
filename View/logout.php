6cf9227545df8c46239423b772cddebfe0e56a58
20-43829-2 <mayeshakhan2k18@gmail.com>
2024-09-10 22:07:07 +0600

    AJAX Added

--- View/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    session_reset();
-    session_destroy();
-    // setcookie("userType", "", time() - (7 * 24 * 3600), "/");
-    // setcookie("id", "", time() - (7 * 24 * 3600), "/");
-    // setcookie("email", "", time() - (7 * 24 * 3600), "/");
-    header('location: login.php');
-?>
--- index.php
+++
@@ -1,3 +0,0 @@
-<?php
-    header("location: View/index.php");
-?>
